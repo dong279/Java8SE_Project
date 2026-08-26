52bf2bb40c95eb14514085d058775fdb984748c3
dong279 <dongho0130@daum.net>
2026-08-26 15:42:57 +0900

    oop good package add

--- src/chapter5/oop/good/Manager.java
+++ src/chapter5/oop/good/Manager.java
@@ -1,9 +1,15 @@
 package chapter5.oop.good;
 /* 관리자 클래스 */
-public class Manager extends Employee {
+public class Manager extends Employee{
+	private String depName;
+	
     public Manager (String name, double salary) {
-        this.name = name;
-        this.salary = salary;
+    	super(name,salary);
+    }
+    
+    public Manager (String name, double salary,String deptName) {
+    	this(name, salary);
+    	this.depName = deptName;
     }
     
     public void manageSalary(double rate) {
