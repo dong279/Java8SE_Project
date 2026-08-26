6431e0441484fc1d50067fd0cd772489c0a45754
dong279 <dongho0130@daum.net>
2026-08-26 13:27:14 +0900

    PersonManager update

--- src/workshop/person/control/PersonManager.java
+++ src/workshop/person/control/PersonManager.java
@@ -45,5 +45,15 @@ public class PersonManager {
 		return genderCnt;
 	}
 	
-	public void showPerson()
+	public void showPerson(PersonEntity[] persons, String name) {
+		for (PersonEntity person : persons) {
+			if(person.getName().equals(name)) {
+				System.out.println("[이름] " + person.getName());
+				System.out.println("[성별] " + person.getGender());
+				System.out.println("[전화번호] " + person.getPhone());
+				System.out.println("[주소] " + person.getAddress());
+				break;
+			}
+		}
+	}
 }
